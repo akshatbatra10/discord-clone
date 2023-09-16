8b13f19abc474fc4fc142f9549d5d3d571bd78d6
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2023-09-16 19:27:01 +0530

    initial commit

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
+import { currentProfile } from "@/lib/current-profile";
+
+const SetupPage = async () => {
+  const profile = await currentProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile?.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
